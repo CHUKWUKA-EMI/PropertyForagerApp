c929dbc8f5272492b52f1ee6d84773883ca88fcd
chukwuka-emi <chukwuka@us.ftft.com>
2023-06-29 20:16:36 +0100

    worked on property details page

--- components/Properties/Property/ContactAgent.tsx
+++ components/Properties/Property/ContactAgent.tsx
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { FC } from "react";
+import { IProperty } from "@/types/property";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextFieldWithoutLabel from "@/components/TextFields/TextFieldWithoutLabel";
+import PrimaryButton from "@/components/Buttons/PrimaryButton";
 
-const ContactAgent = () => {
-  return <div>ContactAgent</div>;
+const ContactAgent: FC<IProperty> = (props) => {
+  return (
+    <Box px={4} py={12} display="flex" gap={6} sx={{ width: "100%" }}>
+      <Box
+        position="relative"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        sx={{
+          width: "30%",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundImage:
+              "linear-gradient(180deg, rgba(40, 46, 56, 0), rgba(40, 46, 56, 0) 51%, rgba(40, 46, 56, 0.75) 72%)",
+            zIndex: 1,
+            position: "absolute",
+            top: "0%",
+            bottom: "0%",
+            left: "0%",
+            right: "0%",
+            borderRadius: "1rem",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            maxWidth: "100%",
+            borderRadius: "1rem",
+            backgroundColor: "transparent",
+          }}
+          component="img"
+          alt="Agent image"
+          src={
+            props.agency ? props.agency.owner.avatarUrl : props.owner?.avatarUrl
+          }
+        ></Box>
+        {props.agency && (
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: 5,
+              p: 2,
+              zIndex: 3,
+            }}
+          >
+            <Typography
+              sx={{ color: "white" }}
+              fontSize={20}
+              fontWeight={900}
+              variant="h5"
+            >
+              {`${props.agency.owner.firstName} ${props.agency.owner.lastName}`}
+            </Typography>
+            <Typography
+              sx={{ color: "GrayText" }}
+              fontSize={19}
+              fontWeight={400}
+              variant="body1"
+            >
+              Your Agent
+            </Typography>
+          </Box>
+        )}
+        {props.owner && (
+          <Box position="absolute">
+            <Typography fontSize={19} fontWeight={500} variant="body1">
+              {`${props.owner.firstName} ${props.owner.lastName}`}
+            </Typography>
+            <Typography
+              color="GrayText"
+              fontSize={19}
+              fontWeight={400}
+              variant="body1"
+            >
+              Property Owner
+            </Typography>
+          </Box>
+        )}
+      </Box>
+      <Box sx={{ width: "50%" }} display="flex" flexDirection="column" gap={3}>
+        <Typography fontWeight={500} variant="h5">
+          Interested in this property? Request a visit.
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+        >
+          <TextFieldWithoutLabel fullWidth placeholder="Enter your name" />
+          <TextFieldWithoutLabel
+            fullWidth
+            type="email"
+            placeholder="Enter your email"
+          />
+          <TextFieldWithoutLabel
+            fullWidth
+            placeholder="Enter your phone number"
+          />
+          <PrimaryButton
+            disableElevation
+            disableRipple
+            sx={{ textTransform: "none", width: "30%", borderRadius: "0.5rem" }}
+            variant="contained"
+            size="large"
+          >
+            Submit
+          </PrimaryButton>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default ContactAgent;
--- components/Properties/Property/ImageGallery.tsx
+++ components/Properties/Property/ImageGallery.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import { IPropertyImage } from "@/types/property";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import React, { FC } from "react";
+import { styled, useTheme } from "@mui/material/styles";
 
-const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+const StyledSvg = styled("svg")(({ theme }) => ({
+  width: "3rem",
+  height: "3rem",
+  position: "absolute",
+  color: "white",
+  backgroundColor: theme.palette.primary.main,
+  display: "none",
+  borderRadius: "5px",
+}));
+
+const ImageGallery: FC<{ images: IPropertyImage[] }> = ({ images }) => {
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{ backgroundColor: theme.palette.primary.main, py: 14, px: 4 }}
+      display="flex"
+      flexDirection="column"
+      gap={6}
+    >
+      <Typography sx={{ color: "white" }} fontWeight={500} variant="h3">
+        Image gallery
+      </Typography>
+      <Grid
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        spacing={4}
+        container
+        overflow="hidden"
+      >
+        {images.map((image, index) => (
+          <Grid
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            overflow="hidden"
+            key={index}
+            item
+            xs={12}
+            md={6}
+            lg={4}
+            sx={{
+              cursor: "pointer",
+              ":hover": {
+                img: {
+                  opacity: 0.8,
+                },
+                svg: {
+                  display: "block",
+                },
+              },
+            }}
+            position="relative"
+          >
+            <Box
+              sx={{
+                display: "inline-block",
+                maxWidth: "100%",
+                verticalAlign: "middle",
+                overflow: "clip",
+                overflowClipMargin: "content-box",
+                transform:
+                  "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                transformStyle: "preserve-3d",
+                ":hover": {
+                  transform:
+                    "translate3d(0px,0px,0px) scale3d(1.1,1.1,1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg,0deg)",
+                  willChange: "transform",
+                },
+                transitionProperty: "transform",
+                transitionTimingFunction: theme.transitions.easing.easeIn,
+                transitionDuration: "0.5s",
+              }}
+              component="img"
+              alt="property image"
+              src={image.imageURL}
+              loading="lazy"
+            ></Box>
+            <StyledSvg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+              />
+            </StyledSvg>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default ImageGallery;
